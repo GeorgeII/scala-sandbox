8d70d96e6d4feb9140c0d32f65b1d5a9c8f2654d
GeorgeII <egorzorikov@gmail.com>
2020-11-25 23:18:36 +0400

    Standard multiplication added

--- concurrent-matrix-multiplication/src/main/scala/com/github/georgeii/Matrix.scala
+++ concurrent-matrix-multiplication/src/main/scala/com/github/georgeii/Matrix.scala
@@ -1,26 +1,49 @@
 package com.github.georgeii
 
-case class Matrix(matrix: Vector[Vector[_ >: Numeric[_]]]) {
+import scala.math.Numeric.Implicits._
+import scala.reflect.ClassTag
 
-  def getRow(idx: Int): Vector[_] = matrix(idx)
+case class Matrix[N : Numeric : ClassTag](matrix: Vector[Vector[N]]) {
 
-  def getColumn(idx: Int): Vector[_] = {
-    for{
-      row <- matrix
-    } yield row(idx)
-  }
+  def getRow(idx: Int): Vector[N] =
+    matrix(idx)
 
-  def transpose: Matrix = {
+  def getColumn(idx: Int): Vector[N] =
+    matrix.map(row => row(idx))
+
+  def transpose: Matrix[N] = {
     val newMatrix = {
-      for {
+      for{
         i <- 0 until this.shape._2
       } yield matrix.map{row => row(i)}
     }.toVector
 
-    Matrix(newMatrix)
+    Matrix[N](newMatrix)
+  }
+
+  def *(other: Matrix[N]): Matrix[Double] = {
+    if (shape._2 != other.shape._1)
+      throw new IllegalArgumentException("Matrices of these shapes cannot be multiplied")
+
+    val result = Array.ofDim[Double](shape._1, other.shape._2)
+
+    for (i <- 0 until shape._1; j <- 0 until other.shape._2) {
+      var aggr = 0
+      val thisRow = this.getRow(i)
+      val otherColumn = other.getColumn(j)
+      result(i)(j) = {
+        for{
+          k <- 0 until shape._2
+        } yield thisRow(k) * otherColumn(k)
+      }.sum.toDouble
+    }
+
+    Matrix(result.map(_.toVector).toVector)
   }
 
   def shape: (Int, Int) = (matrix.size, matrix(0).size)
+
+  override def toString: String = s"Matrix of $matrix:\n" + matrix.map{_.mkString("\t")}.mkString("\n")
 }
 
 
