6eeec1ff79457719dafac780ef14e9ba2d52c3fe
GeorgeII <egorzorikov@gmail.com>
2021-02-18 15:26:39 +0400

    increased the chunk-size and slightly changed the logic of stream processing

--- grep-utility/src/main/scala/Grep.scala
+++ grep-utility/src/main/scala/Grep.scala
@@ -3,36 +3,65 @@ import akka.stream.scaladsl._
 import akka.util.ByteString
 
 import java.nio.file.Paths
+import scala.io.StdIn.readLine
 
 object Grep {
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 2)
-      sys.error("You must pass 2 arguments: a word to find and a file")
+
+    println("Enter a word to find:")
+    val wordToFind = readLine()
+    println("Enter filename:")
+    val filename = readLine()
 
     implicit val system: ActorSystem = ActorSystem("QuickStart")
     import system.dispatcher
 
-    val wordToFind = args(0)
-    val filename = args(1)
-
-    val source = FileIO.fromPath(Paths.get(filename), chunkSize = 1)
+    // a default chunk-size is 8192
+    val source = FileIO.fromPath(Paths.get(filename))
 
     // this is a simple example of reading bytes by fixed-sized frames and waiting for a line separator.
+    // it does not fit well 'cause it has a maximumFrameLength.
 //    val flow = Framing
 //      .delimiter(ByteString(System.lineSeparator()), maximumFrameLength = 512, allowTruncation = true)
 //      .map(_.utf8String)
 
 
-    val sink = Sink.fold[ByteString, ByteString](ByteString("")) {
-      case (line, inputChunk) if inputChunk == ByteString("\n") =>
-        if (line.utf8String.contains(wordToFind))
-          println(line.utf8String)
-        // start accumulating from an empty string again
-        ByteString("")
+    val sink = Sink.fold[String, ByteString]("") {
 
-      case (line, inputChunk) =>
-        line ++ inputChunk
+      case (previousString, inputChunk) =>
+
+        val strings = inputChunk
+          .utf8String
+          .split("\n")
+          .toVector
+
+        // handling a corner case: for instance, we are looking for a 'computer'. There is a possibility of reading a
+        // file in the following chunk-order: '... this is my com' and 'puter\nAnd this is ...'. To print the entire line
+        // we have to get the beginning of a string from the previous chunk and concatenate it with its continuation
+        // in the next chunk.
+        val firstString = previousString + strings(0)
+        val lastString = strings(strings.length - 1)
+
+        val preparedStringsToFindWord = firstString +: strings.slice(1, strings.length - 1)
+
+        for {
+          string <- preparedStringsToFindWord if string.contains(wordToFind)
+        } println(string)
+
+        // another corner case but for the last string in a chunk. If the string does not end with the '\n', we have to
+        // concatenate it with the first string during the next step of 'fold'.
+        var stringToPassToNextIteration = ""
+
+        if (inputChunk(inputChunk.length - 1) == '\n') {
+          if (lastString.contains(wordToFind))
+            println(lastString)
+        }
+        else {
+          stringToPassToNextIteration = lastString
+        }
+
+        stringToPassToNextIteration
     }
 
     source
