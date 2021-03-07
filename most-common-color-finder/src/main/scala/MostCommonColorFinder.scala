11a5e5b051d9fbaeeaf6667b4a05b54f59b31f15
GeorgeII <egorzorikov@gmail.com>
2021-03-07 18:48:21 +0400

    Parallel running + methods organazing added

--- most-common-color-finder/src/main/scala/MostCommonColorFinder.scala
+++ most-common-color-finder/src/main/scala/MostCommonColorFinder.scala
@@ -10,7 +10,26 @@ import scala.concurrent.ExecutionContext
 
 object MostCommonColorFinder {
 
-  case class RGBPixel(red: Int, green: Int, blue: Int)
+  case class RGBPixel(red: Int, green: Int, blue: Int) {
+
+    def color: Int = {
+      red * 0x10000 + green * 0x100 + blue
+    }
+  }
+
+  object RGBPixel {
+    /**
+     * Applies a formula to get red, green, and blue from a pixel of a BufferedImage picture.
+     * @return (R, G, B)
+     */
+    def apply(color: Int): RGBPixel = {
+      val red = (color & 0xff0000) / 0x10000
+      val green = (color & 0xff00) / 0x100
+      val blue = (color & 0xff)
+
+      RGBPixel(red, green, blue)
+    }
+  }
 
   def processFilesPerThread(files: Vector[File]): IO[Vector[RGBPixel]] = {
     files.map(file => getMostFrequentPixelInImage(file)).sequence
@@ -28,7 +47,7 @@ object MostCommonColorFinder {
       for (y <- 0 until height) {
         val pixelMixedColors: Int = image.getRGB(x, y)
 
-        val pixel: RGBPixel = extractRedGreenBlueFromPixel(pixelMixedColors)
+        val pixel: RGBPixel = RGBPixel(pixelMixedColors)
 
         if (pixels.contains(pixel))
           pixels(pixel) += 1
@@ -54,25 +73,14 @@ object MostCommonColorFinder {
   }
 
   /**
-   * Applies a formula to get red, green, and blue from a pixel of a BufferedImage picture.
-   * @return (R, G, B)
-   */
-  def extractRedGreenBlueFromPixel(color: Int): RGBPixel = {
-    val red = (color & 0xff0000) / 0x10000
-    val green = (color & 0xff00) / 0x100
-    val blue = (color & 0xff)
-
-    RGBPixel(red, green, blue)
-  }
-
-  /**
    * Function has side-effects as it writes a new file.
    */
-  def paintMostFrequentPixels(pixels: Vector[RGBPixel], pathName: String): IO[Unit] = IO {
+  def paintMostFrequentPixels(pixels: Vector[RGBPixel], pathName: String): IO[Unit] = {
     val bufferedImageToPaint = makeBufferedImage(pixels)
 
+    // this is where we have side-effects.
     writeImage(bufferedImageToPaint, pathName)
-  }.flatten
+  }
 
   def makeBufferedImage(pixels: Vector[RGBPixel]): BufferedImage = {
     val pixelsOfStripePerSample = 3
@@ -85,17 +93,13 @@ object MostCommonColorFinder {
     for (x <- 0 until width by pixelsOfStripePerSample)
       for (y <- 0 until height) {
         val idx = x / pixelsOfStripePerSample
-        val color = combineRedGreenBlueIntoOne(pixels(idx))
+        val color = pixels(idx).color
 
         for (i <- 0 until pixelsOfStripePerSample)
           outputImage.setRGB(x + i, y, color)
       }
 
     outputImage
-  }
-
-  def combineRedGreenBlueIntoOne(pixel: RGBPixel): Int = {
-    pixel.red * 0x10000 + pixel.green * 0x100 + pixel.blue
   }
 
   def writeImage(image: BufferedImage, pathName: String): IO[Unit] = IO {
@@ -120,12 +124,8 @@ object MostCommonColorFinder {
 
     val filesPerThread = pictures.grouped(numberOfFilesPerThread).toVector
 
-    // Vector of IO to IO of Vector
-    val ioPixelPerThread = {
-      for {
-        files <- filesPerThread
-      } yield IO.shift *> cpuEval(processFilesPerThread(files))
-    }.sequence
+    // Running on a list in parallel.
+    val ioPixelPerThread = filesPerThread.parTraverse(processFilesPerThread)
 
     // concatenate results of each thread into one Vector. It's done inside of IO.
     val ioPixelForEveryPicture = ioPixelPerThread.map(_.flatten)
