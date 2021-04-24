6cd1fa801ebed29e0c602ea509fc0efb3c93804d
GeorgeII <egorzorikov@gmail.com>
2021-04-24 20:10:08 +0400

    Monad chapter

--- functional-programming/src/main/scala/redBook/chapter11/Monads.scala
+++ functional-programming/src/main/scala/redBook/chapter11/Monads.scala
@@ -54,6 +54,25 @@ trait Monad[F[_]] extends Functor[F] {
   }
 }
 
+case class Id[A](value: A) {
+  def map[B](ma: Id[A])(f: A => B): Id[B] = Id(f(value))
+
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+}
+
+case class State[S, A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    State(s => {
+      val (a, s1) = run(s)
+      (f(a), s1)
+    })
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
+}
+
 
 object Monads {
 
@@ -93,6 +112,21 @@ object Monads {
       override def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = {
         ma.flatMap(f)
       }
+    }
+
+
+    type IntState[A] = State[Int, A]
+
+    object IntStateMonad extends Monad[IntState] {
+      def unit[A](a: => A): IntState[A] = State(s => (a, s))
+      def flatMap[A,B](st: IntState[A])(f: A => IntState[B]): IntState[B] =
+        st flatMap f
+    }
+
+    def stateMonad[S] = new Monad[({type f[x] = State[S,x]})#f] {
+      def unit[A](a: => A): State[S,A] = State(s => (a, s))
+      def flatMap[A,B](st: State[S,A])(f: A => State[S,B]): State[S,B] =
+        st flatMap f
     }
 
   }
