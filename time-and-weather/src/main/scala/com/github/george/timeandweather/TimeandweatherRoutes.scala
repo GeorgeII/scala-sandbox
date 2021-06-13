bf8197b2853c7ce4947a148a2e47bf1d882d09a3
GeorgeII <egorzorikov@gmail.com>
2021-06-13 17:10:57 +0400

    Auto encoders and decoders for case classes

--- time-and-weather/src/main/scala/com/github/george/timeandweather/TimeandweatherRoutes.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/TimeandweatherRoutes.scala
@@ -1,11 +1,16 @@
 package com.github.george.timeandweather
 
-import cats.effect.Sync
+import cats.effect.{Sync, Timer}
 import cats.implicits._
-import io.circe.Json
+import fs2.Stream
+import io.circe.generic.auto._
+import io.circe.syntax._
 import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.circe.jsonEncoder
 import org.http4s.dsl.Http4sDsl
+
+import scala.concurrent.duration.DurationInt
 
 object TimeandweatherRoutes {
 
@@ -40,12 +45,34 @@ object TimeandweatherRoutes {
     HttpRoutes.of[F] {
       case GET -> Root / "time" / city =>
         for {
-          timeOrErr <- T.get(city)
+          timeOrErr <- T.get(city.toUpperCase)
           resp      <- timeOrErr match {
-            case Right(time) => Ok(time)
-            case Left(error) => BadRequest(Json.obj("error" -> Json.fromString(error.e.getMessage)))
+            case Right(time) => Ok(time.asJson)
+            case Left(error) => BadRequest(error.asJson)
           }
         } yield resp
+    }
+  }
+
+  def timeStreamingRoutes[F[_]: Sync : Timer](T: Times[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+
+    HttpRoutes.of[F] {
+      case GET -> Root / "streaming" / city =>
+
+        val throttling = Stream.fixedDelay[F](1.second)
+
+        val payload = Stream.emit(T.get(city)).map(timeOrErrF => {
+          timeOrErrF.map {
+            case Right(time) => time.asJson //time
+            case Left(error) => error.asJson
+          }
+        })
+
+        val throttlingPayload = throttling.zip(payload)
+
+        Ok(throttlingPayload.map(_.toString))
     }
   }
 }
