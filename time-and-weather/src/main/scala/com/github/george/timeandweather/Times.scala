dcf7704787107a9e8627dab0f8c798ef27b3a17b
GeorgeII <egorzorikov@gmail.com>
2021-06-13 17:09:26 +0400

    All cities to upper case

--- time-and-weather/src/main/scala/com/github/george/timeandweather/Times.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/Times.scala
@@ -5,6 +5,7 @@ import cats.implicits._
 import cats.effect.Sync
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.generic.auto._
 import org.http4s.{EntityDecoder, EntityEncoder}
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
 
@@ -19,15 +20,22 @@ object Times {
   def apply[F[_]](implicit ev: Times[F]): Times[F] = ev
 
   final case class CurrentTime(city: String, time: LocalDateTime)
+  final case class CurrentTimeError(exception: String) extends RuntimeException
 
-  object CurrentTime {
-    implicit val decoderCurrentTime: Decoder[CurrentTime] = deriveDecoder[CurrentTime]
-    implicit def entityDecoderCurrentTime[F[_]: Sync]: EntityDecoder[F, CurrentTime] = jsonOf
-    implicit val encoderCurrentTime: Encoder[CurrentTime] = deriveEncoder[CurrentTime]
-    implicit def entityEncoderCurrentTime[F[_]: Applicative]: EntityEncoder[F, CurrentTime] = jsonEncoderOf
+  object Codecs {
+//    implicit val decoderCurrentTime: Decoder[CurrentTime] = deriveDecoder[CurrentTime]
+//    implicit def entityDecoderCurrentTime[F[_]: Sync]: EntityDecoder[F, CurrentTime] = jsonOf
+//
+//    implicit val encoderCurrentTime: Encoder[CurrentTime] = deriveEncoder[CurrentTime]
+//    implicit def entityEncoderCurrentTime[F[_]: Applicative]: EntityEncoder[F, CurrentTime] = jsonEncoderOf
+
+
+//    implicit val decoderCurrentTimeError: Decoder[CurrentTimeError] = deriveDecoder[CurrentTimeError]
+//    implicit def entityDecoderCurrentTimeError[F[_]: Sync]: EntityDecoder[F, CurrentTimeError] = jsonOf
+//
+//    implicit val encoderCurrentTimeError: Encoder[CurrentTimeError] = deriveEncoder[CurrentTimeError]
+//    implicit def entityEncoderCurrentTimeError[F[_]: Applicative]: EntityEncoder[F, CurrentTimeError] = jsonEncoderOf
   }
-
-  final case class CurrentTimeError(e: Throwable) extends RuntimeException
 
   def impl[F[_]: Applicative]: Times[F] = new Times[F] {
     override def get(city: String): F[Either[CurrentTimeError, CurrentTime]] = {
@@ -37,12 +45,12 @@ object Times {
 
   private[this] def getLocalDateTime(city: String): Either[CurrentTimeError, CurrentTime] = {
     val region = city match {
-      case "Moscow"      => Right("Europe/Moscow")
-      case "London"      => Right("Europe/London")
-      case "Sydney"      => Right("Australia/Sydney")
-      case "New-York"    => Right("America/New_York")
-      case "Los-angeles" => Right("America/Los_Angeles")
-      case str @ _ => Left(CurrentTimeError(new NoSuchElementException(s"$str city is not supported.")))
+      case "MOSCOW"      => Right("Europe/Moscow")
+      case "LONDON"      => Right("Europe/London")
+      case "SYDNEY"      => Right("Australia/Sydney")
+      case "NEW-YORK"    => Right("America/New_York")
+      case "LOS-ANGELES" => Right("America/Los_Angeles")
+      case str @ _       => Left(CurrentTimeError(s"$str city is not supported yet."))
     }
 
     region.map { timeZone =>
