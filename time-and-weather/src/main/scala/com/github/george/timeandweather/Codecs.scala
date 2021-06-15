99798e9099c09eea2f677209fe53f35796cc877f
GeorgeII <egorzorikov@gmail.com>
2021-06-15 21:12:26 +0400

    An execution context added

--- time-and-weather/src/main/scala/com/github/george/timeandweather/Codecs.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/Codecs.scala
@@ -1,27 +1,33 @@
 package com.github.george.timeandweather
 
+import cats.effect.{IO, Timer}
 import com.github.george.timeandweather.Times.{CurrentTime, CurrentTimeError}
-import cats.effect.IO
 import fs2.Stream
 import io.circe.Encoder
-import io.circe.generic.semiauto.deriveEncoder
 import io.circe.generic.auto._
+import io.circe.generic.semiauto.deriveEncoder
+import org.http4s.EntityEncoder
 import org.http4s.circe.streamJsonArrayEncoderOf
-import org.http4s.dsl.Http4sDsl
-import org.http4s.{EntityEncoder, HttpRoutes}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Codecs {
 
   object Time {
 
-    private implicit final val CirceEncoder: Encoder[CurrentTime] =
-      deriveEncoder
+    val executionContext: ExecutionContext = implicitly[ExecutionContext]
+    implicit val timerEncoder: Timer[IO] = IO.timer(executionContext)
+    
+    implicit val CirceEncoder: Encoder[CurrentTime] =
+    deriveEncoder
 
-    private implicit final val timeOrErrorCirceEncoder: Encoder[Either[CurrentTimeError, CurrentTime]] =
-      Encoder.encodeEither[CurrentTimeError, CurrentTime](leftKey = "error", rightKey = "time")
+    implicit val timeOrErrorCirceEncoder: Encoder[Either[CurrentTimeError, CurrentTime]] =
+    Encoder.encodeEither[CurrentTimeError, CurrentTime](leftKey = "error", rightKey = "time")
 
-    private implicit final val timeOrErrorEntityEncoder: EntityEncoder[IO, Stream[IO, Either[CurrentTimeError, CurrentTime]] =
-      streamJsonArrayEncoderOf
+    implicit val timeOrErrorEntityEncoder: EntityEncoder[IO, Stream[IO, Either[CurrentTimeError, CurrentTime]]] =
+    streamJsonArrayEncoderOf
+
   }
 
 }
