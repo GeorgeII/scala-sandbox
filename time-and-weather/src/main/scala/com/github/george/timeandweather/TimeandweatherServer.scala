c7556db755c11733a77b2d12d3e71d7422ef28db
GeorgeII <egorzorikov@gmail.com>
2021-06-16 22:25:33 +0400

    Explicitly replaced effect F with IO and context shift added

--- time-and-weather/src/main/scala/com/github/george/timeandweather/TimeandweatherServer.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/TimeandweatherServer.scala
@@ -1,38 +1,40 @@
 package com.github.george.timeandweather
 
-import cats.effect.{ConcurrentEffect, Timer}
+import cats.effect.{ConcurrentEffect, IO, Timer}
 import cats.implicits._
 import fs2.Stream
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
+
 import scala.concurrent.ExecutionContext.global
+import com.github.george.timeandweather.Codecs.Time.cs
 
 object TimeandweatherServer {
 
-  def stream[F[_]: ConcurrentEffect](implicit T: Timer[F]): Stream[F, Nothing] = {
+  def stream[F[_]: ConcurrentEffect](implicit T: Timer[IO]): Stream[IO, Nothing] = {
     for {
-      client <- BlazeClientBuilder[F](global).stream
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
-      timeAlg = Times.impl[F]
+      client <- BlazeClientBuilder[IO](global).stream
+      helloWorldAlg = HelloWorld.impl[IO]
+      jokeAlg = Jokes.impl[IO](client)
+      timeAlg = Times.impl[IO]
 
       // Combine Service Routes into an HttpApp.
       // Can also be done via a Router if you
       // want to extract a segments not checked
       // in the underlying routes.
       httpApp = (
-        TimeandweatherRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        TimeandweatherRoutes.jokeRoutes[F](jokeAlg)             <+>
-        TimeandweatherRoutes.timeRoutes(timeAlg)                <+>
+        TimeandweatherRoutes.helloWorldRoutes(helloWorldAlg) <+>
+        TimeandweatherRoutes.jokeRoutes(jokeAlg)             <+>
+        TimeandweatherRoutes.timeRoutes(timeAlg)             <+>
         TimeandweatherRoutes.timeStreamingRoutes(timeAlg)
       ).orNotFound
 
       // With Middlewares in place
       finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
 
-      exitCode <- BlazeServerBuilder[F](global)
+      exitCode <- BlazeServerBuilder[IO](global)
         .bindHttp(8080, "0.0.0.0")
         .withHttpApp(finalHttpApp)
         .serve
