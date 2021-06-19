7dc8d1d585e77c4d41cb814c2a93cb45cda8b11f
GeorgeII <egorzorikov@gmail.com>
2021-06-19 18:56:59 +0400

    Weather request initial logic added

--- time-and-weather/src/main/scala/com/github/george/timeandweather/Weather.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/Weather.scala
@@ -13,4 +13,29 @@ object Weather {
   final case class CurrentWeather(city: String, temperature: Double, windSpeed: Double)
   final case class CurrentWeatherError(error: String)
 
+  private val supportedCities = List("LONDON", "NEW-YORK", "MOSCOW", "LOS-ANGELES", "SYDNEY")
+
+  def impl: Weather = new Weather {
+    override def get(city: String): IO[Either[CurrentWeather, CurrentWeatherError]] = {
+      getLocalWeather(city)
+    }
+  }
+
+  private def getLocalWeather(city: String): IO[Either[CurrentWeather, CurrentWeatherError]] = {
+    val resultCity =
+      if (supportedCities.contains(city.toUpperCase))
+        Left(CurrentWeatherError(s"$city city is not supported yet."))
+      else
+        Right(city.toUpperCase)
+
+    // making an http request here. So, IO encompasses it because this is a side effect.
+    IO {
+      resultCity.map { city =>
+        // TODO: an http request should be made here.
+        // It must be checked whether it was successful and it it wasn't - return Left with a
+        // convenient error message.
+      }
+    }
+  }
+
 }
