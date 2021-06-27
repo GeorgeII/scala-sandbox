50e6ca6f94bd8933df182ec799be0a416b36c962
GeorgeII <egorzorikov@gmail.com>
2021-06-27 11:14:04 +0400

    Encoder for case class added so that Either can be encoded automatically

--- time-and-weather/src/main/scala/com/github/george/timeandweather/Codecs.scala
+++ time-and-weather/src/main/scala/com/github/george/timeandweather/Codecs.scala
@@ -1,9 +1,10 @@
 package com.github.george.timeandweather
 
-import cats.effect.{ConcurrentEffect, ContextShift, IO, Timer}
+import cats.effect.{ContextShift, IO, Timer}
 import com.github.george.timeandweather.Times.{CurrentTime, CurrentTimeError}
+import com.github.george.timeandweather.Weather.{CurrentWeather, CurrentWeatherError}
 import fs2.Stream
-import io.circe.Encoder
+import io.circe.{Encoder, Json}
 import io.circe.generic.auto._
 import io.circe.generic.semiauto.deriveEncoder
 import org.http4s.EntityEncoder
@@ -20,8 +21,7 @@ object Codecs {
     implicit val timerEncoder: Timer[IO] = IO.timer(executionContext)
     implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
 
-    implicit val circeEncoder: Encoder[CurrentTime] =
-      deriveEncoder
+    implicit val circeEncoder: Encoder[CurrentTime] = deriveEncoder
 
     implicit val timeOrErrorCirceEncoder: Encoder[Either[CurrentTimeError, CurrentTime]] =
       Encoder.encodeEither[CurrentTimeError, CurrentTime](leftKey = "error", rightKey = "currentTime")
@@ -29,6 +29,19 @@ object Codecs {
     implicit val timeOrErrorEntityEncoder: EntityEncoder[IO, Stream[IO, Either[CurrentTimeError, CurrentTime]]] =
       streamJsonArrayEncoderOf
 
+  }
+
+  object Weather {
+    implicit val currentWeatherEncoder: Encoder[CurrentWeather] = new Encoder[CurrentWeather] {
+      override def apply(a: CurrentWeather): Json = Json.fromString(a.details)
+    }
+
+    implicit val weatherErrorEncoder: Encoder[CurrentWeatherError] = new Encoder[CurrentWeatherError] {
+      override def apply(a: CurrentWeatherError): Json = Json.fromString(a.error)
+    }
+
+    implicit val weatherEitherCirceEncoder: Encoder[Either[CurrentWeatherError, CurrentWeather]] =
+      Encoder.encodeEither[CurrentWeatherError, CurrentWeather](leftKey = "error", rightKey = "currentWeather")
   }
 
 }
